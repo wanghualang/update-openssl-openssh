c10960e1f31318fe23876efed9f54e780f67ca7a
王花郎 <hwoarang@wanghualang.com>
2019-03-31 21:50:23 +0800

    Update update-openssl-openssh.sh

--- update-openssl-openssh.sh
+++ update-openssl-openssh.sh
@@ -3,6 +3,7 @@ clear
 
 #脚本变量
 date=`date "+%Y%m%d"`
+system_version=`cat /etc/redhat-release`
 prefix="/usr/local"
 zlib_version="zlib-1.2.11"
 dropbear_version="dropbear-2018.76"
@@ -12,18 +13,19 @@ zlib_download="http://zlib.net/$zlib_version.tar.gz"
 dropbear_download="https://matt.ucc.asn.au/dropbear/releases/$dropbear_version.tar.bz2"
 openssl_download="https://www.openssl.org/source/$openssl_version.tar.gz"
 openssh_download="https://openbsd.hk/pub/OpenBSD/OpenSSH/portable/$openssh_version.tar.gz"
+rhel3_version=`cat /etc/redhat-release | grep "release 3" | wc -l`
 rhel4_version=`cat /etc/redhat-release | grep "release 4" | wc -l`
 rhel5_version=`cat /etc/redhat-release | grep "release 5" | wc -l`
 rhel6_version=`cat /etc/redhat-release | grep "release 6" | wc -l`
 rhel7_version=`cat /etc/redhat-release | grep "release 7" | wc -l`
-gcc_intall_status==`rpm -qa | grep -w gcc | wc -l`
-pam_devel_intall_status==`rpm -qa | grep pam-devel | wc -l`
-zlib_devel_intall_status==`rpm -qa | grep zlib-devel | wc -l`
-bzip2_intall_status=`rpm -qa | grep bzip2 | wc -l`
-wget_intall_status=`rpm -qa | grep wget | wc -l`
-make_intall_status=`rpm -qa | grep make | wc -l`
-openssh_rpm_status=`rpm -qa | grep -w openssh-server | wc -l`
-telnet_rpm_status=`rpm -qa | grep -w telnet-server | wc -l`
+gcc_intall_status=`rpm -qa | grep -w gcc | wc -l`
+pam_devel_intall_status=`rpm -qa | grep -w "pam-devel" | wc -l`
+zlib_devel_intall_status=`rpm -qa | grep -w "zlib-devel" | wc -l`
+bzip2_intall_status=`rpm -qa | grep -w "bzip2" | wc -l`
+wget_intall_status=`rpm -qa | grep -w "wget" | wc -l`
+make_intall_status=`rpm -qa | grep -w "make" | wc -l`
+openssh_rpm_status=`rpm -qa | grep -w "openssh-server" | wc -l`
+telnet_rpm_status=`rpm -qa | grep -w "telnet-server" | wc -l`
 telnet_running_status=`ps aux | grep -w "xinetd" | grep -v grep | wc -l`
 dropbear_running_status=`ps aux | grep -w "/usr/local/sbin/dropbear" | grep -v grep | wc -l`
 openssh_running_status=`ps aux | grep -w "/usr/sbin/sshd" | grep -v grep | wc -l`
@@ -36,21 +38,33 @@ sleep 5
 exit
 fi
 
+#检查系统
+if [ "$rhel3_version" == "1" ];then
+clear
+echo -e "脚本仅支持操作系统4.x-7.x版本，五秒后自动退出脚本" "\033[31m Failure\033[0m"
+echo ""
+sleep 5
+exit
+fi
+
 #使用说明
 echo -e "\033[33m软件升级 · 脚本说明\033[0m"
 echo ""
-echo "A.脚本仅适用于RHEL和CentOS操作系统，支持3.x-7.x版本；"
+echo "A.脚本仅适用于RHEL和CentOS操作系统，支持4.x-7.x版本；"
 echo "B.必须使用Root用户运行脚本，确保本机已配置好软件仓库；"
 echo "C.4.x - 5.x操作系统会临时安装Telnet，服务端口为23；"
 echo "D.6.x - 7.x操作系统会临时安装DropBear，服务端口为6666；"
 echo "E.旧版本OpenSSH相关文件备份在/tmp/backup_$date/openssh。"
 echo ""
 
+echo "本机操作系统：$system_version"
+echo ""
+
 #停用SElinux
 setenforce 0 > /dev/null 2>&1
 
 #停用防火墙
-if [ $rhel7_version == 1 ];then
+if [ "$rhel7_version" == "1" ];then
 systemctl stop firewalld > /dev/null 2>&1
 else
 service iptables stop > /dev/null 2>&1
@@ -61,7 +75,11 @@ fi
 function update() {
 
 #创建备份目录
-mkdir -p /tmp/backup_$date/openssh > /dev/null 2>&1
+mkdir -p /tmp/backup_$date/openssh
+mkdir -p /tmp/backup_$date/openssh/usr/{bin,sbin}
+mkdir -p /tmp/backup_$date/openssh/etc/{init.d,pam.d,ssh}
+mkdir -p /tmp/backup_$date/openssh/usr/libexec/openssh
+mkdir -p /tmp/backup_$date/openssh/usr/share/man/{man1,man8}
 
 #安装基础包
 yum -y install gcc pam-devel bzip2 wget make > /dev/null 2>&1
@@ -74,7 +92,7 @@ make > /dev/null 2>&1
 make install > /dev/null 2>&1
 echo "$prefix/$zlib_version/lib" >> /etc/ld.so.conf
 ldconfig
-if [ $gcc_intall_status != 0 ] && [ $pam_devel_intall_status != 0 ] && [ $bzip2_intall_status != 0 ] && [ $wget_intall_status != 0 ] && [ $make_intall_status != 0 ] && [ -e $prefix/$zlib_version/lib/libz.so ];then
+if [ "$gcc_intall_status" != "0" ] && [ "$pam_devel_intall_status" != "0" ] && [ "$bzip2_intall_status" != "0" ] && [ "$wget_intall_status" != "0" ] && [ "$make_intall_status" != "0" ] && [ -e $prefix/$zlib_version/lib/libz.so ];then
 echo -e "安装软件依赖包成功" "\033[32m Success\033[0m"
 else
 echo -e "安装软件依赖包失败，五秒后自动退出脚本" "\033[31m Failure\033[0m"
@@ -103,7 +121,7 @@ fi
 echo ""
 
 #安装远程软件
-if [ $rhel4_version == 1 ] || [ $rhel5_version == 1 ];then
+if [ "$rhel4_version" == "1" ] || [ "$rhel5_version" == "1" ];then
 yum -y install xinetd telnet-server > /dev/null 2>&1
 sed -i '/disable/d' /etc/xinetd.d/telnet
 sed -i '/log_on_failure/a disable  = no' /etc/xinetd.d/telnet
@@ -113,7 +131,7 @@ mv /etc/securetty /etc/securetty.bak_$date
 service xinetd restart > /dev/null 2>&1
 fi
 
-if [ $rhel6_version == 1 ] || [ $rhel7_version == 1 ];then
+if [ "$rhel6_version" == "1" ] || [ "$rhel7_version" == "1" ];then
 cd /tmp
 tar xjf $dropbear_version.tar.bz2
 cd $dropbear_version
@@ -127,15 +145,11 @@ mkdir /etc/dropbear
 fi
 
 #备份旧版OpenSSH
-mkdir -p /tmp/backup_$date/openssh/usr/{bin,sbin}
-mkdir -p /tmp/backup_$date/openssh/etc/{init.d,pam.d,ssh}
-mkdir -p /tmp/backup_$date/openssh/usr/share/man/{man1,man8}
-mkdir -p /tmp/backup_$date/openssh/usr/libexec/openssh
 rpm -ql openssh > /tmp/backup_$date/openssh/openssh-rpm-backup-list.txt
 rpm -ql openssh-server > /tmp/backup_$date/openssh/openssh-server-rpm-backup-list.txt
 find / -name "ssh*" > /tmp/backup_$date/openssh/openssh-backup-list.txt
 
-if [ $openssh_rpm_status != 0 ];then
+if [ "$openssh_rpm_status" != "0" ];then
 cp /usr/bin/ssh* /tmp/backup_$date/openssh/usr/bin > /dev/null 2>&1
 cp /usr/sbin/sshd /tmp/backup_$date/openssh/usr/sbin > /dev/null 2>&1
 cp /etc/init.d/sshd /tmp/backup_$date/openssh/etc/init.d > /dev/null 2>&1
@@ -207,8 +221,7 @@ cp -rf /tmp/$openssh_version/contrib/redhat/sshd.pam /etc/pam.d/sshd
 chmod +x /etc/init.d/sshd
 chkconfig --add sshd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-if [ $rhel7_version == 1 ];then
+if [ "$rhel7_version" == "1" ];then
 chmod 600 /etc/ssh/ssh_host_rsa_key
 chmod 600 /etc/ssh/ssh_host_ecdsa_key
 chmod 600 /etc/ssh/ssh_host_ed25519_key
@@ -217,8 +230,12 @@ else
 service sshd start > /dev/null 2>&1
 fi
 
-if [ $openssh_running_status != 0 ];then
+if [ "$openssh_running_status" != "0" ];then
 echo -e "启动OpenSSH服务成功" "\033[32m Success\033[0m"
+echo ""
+$prefix/$openssl_version/bin/openssl version
+echo ""
+ssh -V
 else
 echo -e "启动OpenSSH服务失败，五秒后自动退出脚本" "\033[31m Failure\033[0m"
 sleep 5
@@ -232,31 +249,23 @@ rm -rf /tmp/$openssh_version*
 rm -rf /tmp/$dropbear_version*
 rm -rf /tmp/$zlib_version*
 
-#升级结果
-echo -e "\033[33mOpenSSL、OpenSSH软件版本：\033[0m"
-echo ""
-$prefix/$openssl_version/bin/openssl version
-echo ""
-ssh -V
-echo ""
-
 #卸载telnet
-if [ $rhel4_version == 1 ] || [ $rhel5_version = 1 ];then
+if [ "$rhel4_version" == "1" ] || [ "$rhel5_version" = "1" ];then
 echo -e "\033[33m为防止OpenSSH升级失败导致无法远程登录，脚本已临时安装Telnet\033[0m"
 echo ""
 echo -e "\033[33mOpenSSH升级完成后，建议登录测试，确保没有问题后可卸载Telnet\033[0m"
 echo ""
-echo "1: 卸载Telnet"
+echo -e "\033[36m1: 卸载Telnet\033[0m"
 echo ""
-echo "2: 退出脚本"
+echo -e "\033[36m2: 退出脚本\033[0m"
 echo ""
 read -p  "请输入对应数字后按回车键: " uninstall
-if [ $uninstall == 1 ];then
+if [ "$uninstall" == "1" ];then
 clear
 yum -y remove telnet-server > /dev/null 2>&1
 service xinetd stop > /dev/null 2>&1
 mv /etc/securetty.bak_$date /etc/securetty
-if [ $telnet_rpm_status == 0 ];then
+if [ "$telnet_rpm_status" == "0" ];then
 echo -e "卸载Telnet成功" "\033[32m Success\033[0m"
 else
 echo -e "卸载Telnet失败，五秒后自动退出脚本" "\033[31m Failure\033[0m"
@@ -267,23 +276,23 @@ fi
 fi
 
 #卸载dropbear
-if [ $rhel6_version == 1 ] || [ $rhel7_version == 1 ];then
+if [ "$rhel6_version" == "1" ] || [ "$rhel7_version" == "1" ];then
 echo -e "\033[33m为防止OpenSSH升级失败导致无法远程登录，脚本已临时安装DropBear\033[0m"
 echo ""
 echo -e "\033[33mOpenSSH升级完成后，建议登录测试，确保没有问题后可卸载DropBear\033[0m"
 echo ""
-echo "1: 卸载DropBear"
+echo -e "\033[36m1: 卸载DropBear\033[0m"
 echo ""
-echo "2: 退出脚本"
+echo -e "\033[36m2: 退出脚本\033[0m"
 echo ""
 read -p  "请输入对应数字后按回车键: " uninstall
-if [ $uninstall == 1 ];then
+if [ "$uninstall" == "1" ];then
 clear
 ps aux | grep dropbear | grep -v grep | awk '{print $2}' | xargs kill -9
 find /usr/local/ -name dropbear* | xargs rm -rf
 rm -rf /etc/dropbear
 rm -rf /var/run/dropbear.pid
-if [ $dropbear_running_status == 0 ];then
+if [ "$dropbear_running_status" == "0" ];then
 echo -e "卸载DropBear成功" "\033[32m Success\033[0m"
 else
 echo -e "卸载DropBear失败，五秒后自动退出脚本" "\033[31m Failure\033[0m"
